361fcc6a2b210deb4a08d34723513b666d113645
SachalBirhmani <117303630+SachalBirhmani@users.noreply.github.com>
2022-11-09 23:15:21 +0500

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+class evenodd{ 
+public static void main (string[] args ) 
+{ 
+ int a;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter any number. for even odd");
+ a=sc.nextInt();
+ if(a%2==0)
+ { 
+   System.out.println("even number.="+a)  
+ } 
+   else 
+  { 
+    System.out.println("odd number.="+a) 
+
+  }
+}
---
+++ variables.java
@@ -0,0 +1,20 @@
+class variable
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=8;
+        System.out.println(a);
+        System.out.println(b);
+    double c=9.4;
+    double d=0.9;
+    System.out.println(c);
+    System.out.println(d);
+    char e='i';
+    char f='a';
+    System.out.println(e);
+    System.out.println(f);
+    String t="Java programs";
+    System.out.println("t");
+    }
+}
